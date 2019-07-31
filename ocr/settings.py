dede75b8410652cb74da411d950fe44b9cf1c692
dodoproptit99 <dodo.proptit.99@gmail.com>
2019-07-31 14:36:28 +0700

    update views, url

--- ocr/settings.py
+++ ocr/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chanels',
+    # 'chanels',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -71,14 +72,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ocr.wsgi.application'
 ASGI_APPLICATION = 'ocr.routing.application'
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
-}
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
