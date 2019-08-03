d26db514fe177e311a3d9b47846180133e379517
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2019-08-03 22:02:36 +0700

    merge box

--- home/static/js/socket_image.js
+++ home/static/js/socket_image.js
@@ -8,7 +8,7 @@ function getParagraph(block){
   }
   return res;
 }
-
+ 
 function getWord(para){
   var res = [];
   var words = para['words'];
@@ -20,7 +20,7 @@ function getWord(para){
   // console.log(res);
   return res;
 }
-
+ 
 function getSym(word){
   var res = '';
   var symbols = word['symbols'];
@@ -29,18 +29,43 @@ function getSym(word){
   }
   return res;
 }
-
+ 
+function merger_text(w) {
+  var res = [];
+  var words = w['words'];
+  var index = 0;
+  while (index <= words.length - 1){
+    var temp = '';
+    temp += words[index]['text'] + ' ';
+    if(index < words.length - 2) {
+      while (Math.abs(words[index]['boundingBox']['vertices'][1]['y'] - words[index+1]['boundingBox']['vertices'][1]['y']) < 8) 
+      {
+        temp += words[index+1]['text'] + ' ';
+        index+=1;
+        console.log(index);
+        if(index >= words.length - 1) {
+          break;
+        }
+      }     
+    }
+    index+=1;
+    res.push({'text': temp});
+  }
+  return res;
+}
+ 
+ 
 function run() {
     endpoint = 'ws://' + window.location.host + '/message/';
     console.log(window.location.host)
     var socket = new ReconnectingWebSocket(endpoint);
-
+ 
     if (window.File && window.FileReader && window.FileList && window.Blob) {
       document.getElementById('files').addEventListener('change', handleFileSelect, false);
     } else {
       alert('The File APIs are not fully supported in this browser.');
     }
-  
+ 
     function handleFileSelect(evt) {
       var f = evt.target.files[0]; 
       var reader = new FileReader();
@@ -59,7 +84,7 @@ function run() {
       // Read in the image file as a data URL.
       reader.readAsBinaryString(f);
     };
-  
+ 
     socket.onmessage = function (e) {
       console.log(e);
       var responseData = JSON.parse(e.data);
@@ -72,13 +97,17 @@ function run() {
         var block = blocks[index];
         words['words'] = words['words'].concat(getParagraph(block)); //bind word from each block together
       }
+ 
       console.log(words);
+      var res=merger_text(words);
+      console.log(res);
+ 
       for (index in words['words']){
         word = words['words'][index];
         $('#result').append($('<li> ' + JSON.stringify(word['boundingBox']['vertices']) + '</br>' + word['text'] + ' </li>'))
       }
     };
-
-
+ 
+ 
   };
   run(); 
