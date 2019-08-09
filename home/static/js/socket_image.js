278c033c1ebc14b9ae438701e5e2d54d29510e19
HanhNguyenDuc <nguyenduchanh99tnvn@gmail.com>
2019-08-09 17:29:11 +0700

    complete export table

--- home/static/js/socket_image.js
+++ home/static/js/socket_image.js
@@ -27,14 +27,14 @@ function getMainPath(ans) {
   var end = 0;
   for (path in ans) {
     for (text in ans[path]) {
-      if (ans[path][text].includes('Hóa đơn bán lẻ')) {
+      if (ans[path][text].includes('Mặt hàng')) {
         // console.log('reached here!');
         start = Number(path) + 1;
         break;
       }
     }
     for (text in ans[path]) {
-      if (ans[path][text].includes('Tổng')) {
+      if (ans[path][text].includes('TỔNG')) {
         // console.log('reached here!');
         end = Number(path);
         break;
@@ -49,40 +49,47 @@ function getMainPath(ans) {
   return res;
 }
 
-function merge(strarr){
+function merge(strarr) {
   var res = '';
-  for (k = 0; k<strarr.length; k++){
+  for (k = 0; k < strarr.length; k++) {
     res += strarr[k] + "";
   }
   return res;
 }
 
-function split_value_line(product_name, line){
+function split_value_line(product_name, line) {
   var product_code = line[0];
   var split = line[1].indexOf('x');
-  var product_value = line[1].slice(0, split);
-  var product_count = line[1].slice(split + 1, line[1].length);
-  var sum = line[2];
-  return {'product_name': merge(product_name), 
-  'product_code': product_code,
-  'product_value': product_value,
-  'product_count': product_count,
-  'sum': sum,
-  'km': 0};
+  var product_value = line[1];
+  var product_count = line[2];
+  var sum = line[3];
+  console.log('type of sum', typeof sum);
+  console.log('type of product_value', typeof product_value);
+  return {
+    'product_name': merge(product_name),
+    'product_code': product_code,
+    'product_value': product_value.replace(/' '/g, ''),
+    'product_count': product_count,
+    'sum': sum,
+    'km': 0
+  };
 }
 
 function getTable(res) {
   //return json object 
   var ans = [];
-  for (i = 0; i<res.length - 1; i++){
-    for (j = 0; j < res[i].length; j++){
+  for (i = 0; i < res.length - 1; i++) {
+    for (j = 0; j < res[i].length; j++) {
       var str = res[i][j];
-      if (str.charCodeAt(0) < 48 || str.charCodeAt(0) > 57){
-        if (res[i+1].length < 3) break;
-        var result = split_value_line(res[i], res[i+1]);
-        if (i + 2 < res.length){
-          if (res[i+2][0].indexOf("КМ") != -1){
-            result['km'] = res[i+2][1];
+      if (str.charCodeAt(0) < 48 || str.charCodeAt(0) > 57) {
+        console.log(Number(res[i+1][0]));
+        if (res[i + 1].length < 3 || Number(res[i + 1][0]) == NaN) break;
+
+        // console.log('reached', res[i][j])
+        var result = split_value_line(res[i], res[i + 1]);
+        if (i + 2 < res.length) {
+          if (res[i + 2][res[i+2].length - 1].charAt(0) == '-') {
+            result['km'] = res[i + 2][res[i+2].length - 1];
             i += 1;
           };
         }
@@ -104,6 +111,34 @@ function getSym(word) {
   }
   return res;
 }
+
+function checkNumber(text){
+  if (Number(text) != NaN) return true;
+}
+//take the boundingbox object
+function merge_box(boxs){
+  // var minx = 10000, maxx = -1, miny = 10000, maxy = -1;
+  var text = '';
+  // console.log(boxs);
+  for (box in boxs){
+    text += boxs[box].text;
+    if (box != boxs.length - 1)
+      text += ' ';
+
+    // minx = Math.min(box.boundingBox['vertices'][0]['x']);
+    // maxx = Math.max(box.boundingBox['vertices'][3]['x']);
+    // miny = Math.min(box.boundingBox['vertices'][0]['y']);
+    // miny = Math.max(box.boundingBox['vertices'][3]['y']);
+  }
+  
+  // var new_box = {'vertices': [
+  //   {'x': minx, 'y': miny},
+  //   {'x': maxx, 'y': miny},
+  //   {'x': maxx, 'y': maxy},
+  //   {'x': minx, 'y': maxy}
+  // ]};
+  return text;
+}
 // Sửa hàm này !
 function merge_text(w) {
   console.log(w);
@@ -116,19 +151,20 @@ function merge_text(w) {
     var y2 = a['boundingBox']['vertices'][3]['y'];
     var x1 = b['boundingBox']['vertices'][3]['x'];
     var x2 = a['boundingBox']['vertices'][3]['x'];
-    if (y1 - y2 > 15) return -1;
-    else if (Math.abs(y1 - y2) <= 15 && x2 < x1) return -1;
-    else if (Math.abs(y1 - y2) <= 15 && x2 > x1) return 0;
+    if (y1 - y2 > 10) return -1;
+    else if (Math.abs(y1 - y2) <= 10 && x2 < x1) return -1;
+    else if (Math.abs(y1 - y2) <= 10 && x2 > x1) return 0;
     else return 1;
   });
+  //Remember always keep the coordinate, that make the world
   var count = 0;
   while (index <= words.length - 1) {
     res.push([]);
-    var temp = words[index]['text'];
+    var temp = words[index];
     res[count].push(temp);
     if (index < words.length - 2) {
-      while (Math.abs(words[index]['boundingBox']['vertices'][3]['y'] - words[index + 1]['boundingBox']['vertices'][3]['y']) < 15) {
-        temp = words[index + 1]['text'];
+      while (Math.abs(words[index]['boundingBox']['vertices'][3]['y'] - words[index + 1]['boundingBox']['vertices'][3]['y']) < 10) {
+        temp = words[index + 1];
         res[count].push(temp);
         index += 1;
         if (index >= words.length - 1) {
@@ -139,51 +175,74 @@ function merge_text(w) {
     index += 1;
     count++;
   }
+  console.log(res);
+  //Merging the fucking text
+  //is under there
+  //stupid patheic
+
   var ans = [];
   var i = 0;
-  // console.log(res);
-  for (i = 0; i < count; i++) {
-    ans.push([]);
-    // console.log(...Array(5).keys());
-    var num = res[i].length;
-    for (k = 0; k < num; k++) {
+  var res2 = [];
 
-      if ((res[i][k].charCodeAt(0) >= 48) && (res[i][k].charCodeAt(0) <= 57)) {
-        if (k + 1 < res[i].length) {
-          if (res[i][k + 1].charAt(0) == '.') {
-            if ((k + 2 < res[i].length) && ((res[i][k + 2].charCodeAt(0) >= 48) && (res[i][k + 2].charCodeAt(0) <= 57))) {
-              var tmp = "";
-              tmp += res[i][k] + "";
-              tmp += res[i][k + 1] + "";
-              tmp += res[i][k + 2] + "";
-              ans[i].push(tmp);
-              k += 2;
-            } else {
-              ans[i].push(res[i][k]);
-            }
-          } else {
-            ans[i].push(res[i][k]);
-          }
+  //merge the texts that are splited by google API first, then use 
+  //another function to merge their coordinates
+  for (i = 0; i<count; i++){
+    res2.push([]);
+    for (j = 0; j < res[i].length; j++){
+      var list_to_merge = [];
+      list_to_merge.push(res[i][j]);
+      while (j < res[i].length - 1){
+        if (Math.abs(res[i][j].boundingBox.vertices[2].x - res[i][j + 1].boundingBox.vertices[3].x) <= 15){
+          list_to_merge.push(res[i][j + 1]);
+          j++;
         }
-      } else {
-        var tmp = "";
-        while (1) {
-          if (k >= res[i].length) {
-            break;
-          }
-          if (res[i][k].charCodeAt(0) >= 48 && res[i][k].charCodeAt(0) <= 57) {
-            k--;
-            break;
-          }
-          tmp += res[i][k] + " ";
-          k++;
+        else{
+          break;
         }
-        ans[i].push(tmp);
       }
+      res2[i].push(merge_box(list_to_merge));
     }
   }
-  console.log(ans);
-  return ans;
+  console.log(res2);
+  // console.log(res);
+  // for (i = 0; i < count; i++) {
+  //   ans.push([]);
+  //   // console.log(...Array(5).keys());
+  //   var num = res[i].length;
+  //   for (k = 0; k < num; k++) {
+  //     if ((res[i][k].charCodeAt(0) >= 48) && (res[i][k].charCodeAt(0) <= 57)) {
+  //       if (k + 1 < res[i].length && k != 0) {
+  //         //Number phase
+  //         var tmp = '';
+  //         var flag = false;
+  //         while (k < res[i].length){
+  //           console.log(tmp);
+  //           tmp += res[i][k];
+  //           if (res[i][k] == '.'){
+  //             tmp += res[i][k + 1];
+  //             break;
+  //           }
+  //         }
+  //       }
+  //     } else {
+  //       var tmp = "";
+  //       while (1) {
+  //         if (k >= res[i].length) {
+  //           break;
+  //         }
+  //         if (res[i][k].charCodeAt(0) >= 48 && res[i][k].charCodeAt(0) <= 57) {
+  //           k--;
+  //           break;
+  //         }
+  //         tmp += res[i][k] + " ";
+  //         k++;
+  //       }
+  //       ans[i].push(tmp);
+  //     }
+  //   }
+  // }
+  // console.log(ans);
+  return res2;
 }
 
 
@@ -223,9 +282,9 @@ function run() {
     console.log(e);
     var responseData = JSON.parse(e.data);
     var file_name = 'data:image/png;base64,' + responseData.base64;
-    $('#images').append($('<div class="clickable_img" onclick="show_img_details(\'' + 
-                    file_name + ' \')"> <img width = \"100\" class="img-fluid img-thumbnail" alt=\"Avatar\"src=\"data:image/png; base64, ' 
-                    + responseData.base64 + '\
+    $('#images').append($('<div class="clickable_img" onclick="show_img_details(\'' +
+      file_name + ' \')"> <img width = \"100\" class="img-fluid img-thumbnail" alt=\"Avatar\"src=\"data:image/png; base64, '
+      + responseData.base64 + '\
                     "></div>'));
     var text_data = JSON.parse(responseData.Text_Description);
     var blocks = text_data['pages'][0]['blocks'];
@@ -242,7 +301,7 @@ function run() {
     $('#detail').append($("<table id = 'table-content'></table>"))
     var table = '<tr><th> Tên sản phẩm </th> <th> Mã sản phẩm </th> <th> Đồng / 1 sản phẩm </th> <th> Số lượng </th> <th> Tổng giá </th> <th> Khuyến mãi </th> </tr>';
     var total_value = 0;
-    for (i = 0; i < tableContent.length; i++){
+    for (i = 0; i < tableContent.length; i++) {
       table += '<tr>';
       table += '<th>' + tableContent[i].product_name + '</th>';
       table += '<th>' + tableContent[i].product_code + '</th>';
